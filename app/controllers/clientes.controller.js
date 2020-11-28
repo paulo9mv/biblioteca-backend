0f5c1f1dcd093e42a633dd289f2ce755ff4cae1d
Paulo Viana <paulo9mv@yahoo.com.br>
2020-11-28 16:43:09 -0400

    Adiciona create cliente

--- app/controllers/clientes.controller.js
+++ app/controllers/clientes.controller.js
@@ -2,8 +2,28 @@ const db = require("../models");
 const Cliente = db.clientes;
 
 exports.create = (req, res) => {
+    if (!req.body.nome) {
+      res.status(400).send({ message: "Erro. Nome obrigatório." });
+      return;
+    }
   
-};
+    const cliente = new Cliente({
+      nome: req.body.nome,
+      email: req.body.email,
+    });
+  
+    cliente
+      .save(cliente)
+      .then(data => {
+        res.send(data);
+      })
+      .catch(err => {
+        res.status(500).send({
+          message:
+            err.message || "Algo errado aconteceu. Tente mais tarde."
+        });
+      });
+  };
 
 exports.findAll = (req, res) => {
   
